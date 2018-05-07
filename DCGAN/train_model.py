c24734bcdc5084060d00433d3fe7996e44474461
Abhishek Tandon <37059580+Tandon-A@users.noreply.github.com>
2018-05-07 15:23:27 +0530

    Added flags for terminal execution

--- DCGAN/train_model.py
+++ DCGAN/train_model.py
@@ -1,14 +1,18 @@
 import tensorflow as tf 
 import numpy as np 
-import matplotlib.pyplot as plt
 from glob import glob
 import random
-from PIL import Image 
+from PIL import Image
+import os  
+import scipy.misc
+
 #import the DCGAN model definition from model definition python file. 
 from dcgan_model import DCGAN
 
 
-#Function to get image from path and rescale them to [-1,1]
+"""
+Function to get image from path and rescale them to [-1,1]
+"""
 def get_image_new(image_path,width,height):
     image = Image.open(image_path)
     #crop image to reduce clutter 
@@ -23,14 +27,17 @@ def get_image_new(image_path,width,height):
     return image
 
 #Train DCGAN 
-def train(net,max_iter,batch_size,data_files,lr_rate,beta1,shape,z_dim):
-    saver = tf.train.Saver()
+def train(net,max_iter,batch_size,data_files,model_dir,image_dir,lr_rate,beta1,shape,z_dim):
+    saver = tf.train.Saver(max_to_keep=None)
+    
     random.shuffle(data_files)
     max_bs_len = int(len(data_files)/batch_size)*batch_size
     
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for epoch in range(max_iter):
+        #saver.restore(sess,model_dir+"try_5_3000\\")
+        epoch = 0
+        while epoch < (max_iter):
             bs = 0
             step = 0
             while bs < max_bs_len:
@@ -55,37 +62,54 @@ def train(net,max_iter,batch_size,data_files,lr_rate,beta1,shape,z_dim):
                         img = (((img - img.min())*255)/(img.max()-img.min())).astype(np.uint8)
                     else:
                         img = ((img - img.min())*255).astype(np.uint8)
-                    plt.imshow(img)
-                    plt.show()
-                
-                if step % 500 == 0:
-                    saver.save(sess,model_dir + "try_" + str(epoch)+"_"+str(step) +"_upd\\")
-                    print ("### Model weights Saved epoch = %r , step = %r ###" %(epoch,step))
                     
+                    scipy.misc.toimage(img, cmin=0.0, cmax=...).save(image_dir+"\\img_"+str(epoch)+"_"+str(step)+".jpg")
                     
                 else: 
-                    print ("calc")
+                    print ("step = %r" %(step))
                 bs = bs + batch_size
                 step = step +1 
-                    
-#Change model_dir to location where you want to save model weights                    
-model_dir = "dcgan\\tf\\model\\"
-batch_size = 64
-z_dim = 100
-lr_rate = 0.0002
-beta1 = 0.5
-alpha = 0.2
-max_iter = 25
+                
+            dir_path =  model_dir + "try_" + str(epoch)+"\\"
+            saver.save(sess,dir_path,write_meta_graph=True)
+            print ("### Model weights Saved epoch = %r  ###" %(epoch))
+            epoch = epoch + 1
+            
+            
+def main(_): 
+      
 
-"""
-Change data_files to location where database is saved. CelebA Face Database is used in this project. 
-"""
-data_files = glob("dataset\\img\\img_align_celeba\\*.jpg")
-shape = len(data_files),64,64,3
-tf.reset_default_graph()
-
-
-net = DCGAN(shape[1:],z_dim,lr_rate,beta1,alpha)
-
-train(net,max_iter,batch_size,data_files,lr_rate,beta1,shape,z_dim)
+    if not os.path.exists(FLAGS.data_path):
+        print ("Training Path doesn't exist")
+    else:
+            
+        if not os.path.exists(FLAGS.model_dir):
+            os.makedirs(FLAGS.model_dir)
+        if not os.path.exists(FLAGS.sampled_images_dir):
+            os.makedirs(FLAGS.sampled_images_dir)
     
+        batch_size = 64
+        z_dim = 100
+        lr_rate = 0.0002
+        beta1 = 0.5
+        alpha = 0.2
+        max_iter = 25
+        #CelebA Face Database is used in this project. 
+        data_files = glob(str(FLAGS.data_path) +"\\"+str(FLAGS.input_fname_pattern))
+        shape = 64,64,3
+        tf.reset_default_graph()
+                
+        net = DCGAN(shape,z_dim,lr_rate,beta1,alpha)
+        
+        train(net,max_iter,batch_size,data_files,FLAGS.model_dir,FLAGS.sampled_images_dir,lr_rate,beta1,shape,z_dim)
+
+
+flags = tf.app.flags
+flags.DEFINE_string("data_path",None,"Directory of the database folder having training images")
+flags.DEFINE_string("input_fname_pattern","*.jpg","Glob pattern of training images")
+flags.DEFINE_string("model_dir","dcgan_model","Directory path to save checkpoints")
+flags.DEFINE_string("sampled_images_dir","sampled_images","Directory where images sampled from the generator (while training the model) are stored")
+FLAGS = flags.FLAGS
+    
+if __name__ == '__main__':
+    tf.app.run()
